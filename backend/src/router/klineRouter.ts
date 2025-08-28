d059f789e5349d95c9ec51b60834bf1455b4bab5
Nagmani001 <nagmanipd3@gmail.com>
2025-08-29 01:44:07 +0545

    asdf

--- backend/src/router/klineRouter.ts
+++ backend/src/router/klineRouter.ts
@@ -1,10 +1,112 @@
+import { Client } from "pg";
 import { Router, Request, Response } from "express";
 
+const pgClient = new Client({
+  host: "localhost",
+  port: 5432,
+  user: "nagmani",
+  password: "nagmani",
+  database: "postgres"
+});
 
 export const klineRouter = Router();
+pgClient.connect();
 
 
-klineRouter.get("/", (req: Request, res: Response) => {
-  const { interval, startTime, endTime } = req.body;
+klineRouter.get("/", async (req: Request, res: Response) => {
+  const { interval, startTime, endTime, market } = req.body;
 
+  let query;
+
+  if (market === "btc_usdt") {
+
+    switch (interval) {
+      case '1m':
+        query = `SELECT * FROM btc_klines_1m WHERE bucket >= $1 AND bucket <= $2`;
+        break;
+      case '1h':
+        query = `SELECT * FROM btc_klines_1h WHERE  bucket >= $1 AND bucket <= $2`;
+        break;
+      case '1w':
+        query = `SELECT * FROM btc_klines_1w WHERE bucket >= $1 AND bucket <= $2`;
+        break;
+      default:
+        return res.status(400).send('Invalid interval');
+    }
+    try {
+      const result = await pgClient.query(query, [new Date(startTime * 1000), new Date(endTime * 1000)]);
+      console.log(result);
+      res.json(result.rows.map(x => {
+        return {
+          open: x.open,
+          close: x.close,
+          high: x.high,
+          low: x.low
+        }
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+
+  } else if (market === "eth_usdt") {
+
+    switch (interval) {
+      case '1m':
+        query = `SELECT * FROM eth_klines_1m WHERE bucket >= $1 AND bucket <= $2`;
+        break;
+      case '1h':
+        query = `SELECT * FROM eth_klines_1h WHERE  bucket >= $1 AND bucket <= $2`;
+        break;
+      case '1w':
+        query = `SELECT * FROM eth_klines_1w WHERE bucket >= $1 AND bucket <= $2`;
+        break;
+      default:
+        return res.status(400).send('Invalid interval');
+    }
+
+    try {
+      const result = await pgClient.query(query, [new Date(startTime * 1000), new Date(endTime * 1000)]);
+      console.log(result);
+      res.json(result.rows.map(x => {
+        return {
+          open: x.open,
+          close: x.close,
+          high: x.high,
+          low: x.low
+        }
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  } else if (market === "sol_usdt") {
+
+    switch (interval) {
+      case '1m':
+        query = `SELECT * FROM sol_klines_1m WHERE bucket >= $1 AND bucket <= $2`;
+        break;
+      case '1h':
+        query = `SELECT * FROM sol_klines_1h WHERE  bucket >= $1 AND bucket <= $2`;
+        break;
+      case '1w':
+        query = `SELECT * FROM sol_klines_1w WHERE bucket >= $1 AND bucket <= $2`;
+        break;
+      default:
+        return res.status(400).send('Invalid interval');
+    }
+
+    try {
+      const result = await pgClient.query(query, [new Date(startTime * 1000), new Date(endTime * 1000)]);
+      console.log(result);
+      res.json(result.rows.map(x => {
+        return {
+          open: x.open,
+          close: x.close,
+          high: x.high,
+          low: x.low
+        }
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  }
 })
